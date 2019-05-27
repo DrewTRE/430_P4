5f05b85d9dbc179e6cdc260d3696cf8c2202ef0d
DrewTRE <43226138+DrewTRE@users.noreply.github.com>
2019-05-27 12:04:44 -0700

    Just more updates...

--- ThreadOS/Cache.java
+++ ThreadOS/Cache.java
@@ -8,22 +8,22 @@ public class Cache {
 	private int 	foundPage; 
     
     private class Entry { 
-		int 		blockFrameNumber;
+		int 		frame;
 		boolean 	refBit;
 		boolean 	dirtyBit;
 		byte[] 		dataBlock;
 	
 		private Entry(int blockSize) {
-			dataBlock 			= new byte[blockSize];
-			blockFrameNumber 	= -1;	
-        	refBit 				= false;	
-        	dirtyBit 			= false;	
+			dataBlock 	= new byte[blockSize];
+			frame 		= -1;	
+        	refBit 		= false;	
+        	dirtyBit 	= false;	
 		}
     }
 
     private int findFreePage() {
 		for (int i = 0; i < pageTable.length; i++) {	
-            if (pageTable[i].blockFrameNumber == -1) {
+            if (pageTable[i].frame == -1) {
 				return i;	
 			}
         }
@@ -32,7 +32,7 @@ public class Cache {
 
 	private int findBlock(int findBlockID) {
 		for (int i = 0; i < pageTable.length; i++) {	
-            if (pageTable[i].blockFrameNumber == findBlockID) {
+            if (pageTable[i].frame == findBlockID) {
 				return i;
 			}
         }
@@ -51,7 +51,7 @@ public class Cache {
 
     private void writeBack(int victimEntry) {
 		if(pageTable[victimEntry].dirtyBit) {
-			SysLib.rawwrite(pageTable[victimEntry].blockFrameNumber, pageTable[victimEntry].dataBlock);
+			SysLib.rawwrite(pageTable[victimEntry].frame, pageTable[victimEntry].dataBlock);
 			pageTable[victimEntry].dirtyBit = false; 
 		}
     }
@@ -70,7 +70,7 @@ public class Cache {
 		}
 		foundPage = findBlock(blockId);				
 		if(foundPage != -1) { 	
-			pageTable[foundPage].blockFrameNumber 	= blockId;	
+			pageTable[foundPage].frame 	= blockId;	
 			System.arraycopy(pageTable[foundPage].dataBlock, 0, buffer, 0, blockSize);	
 			pageTable[foundPage].refBit 			= true; 
 			return true; 
@@ -78,15 +78,15 @@ public class Cache {
 		foundPage = findFreePage();					
 		if(foundPage != -1) { 	
 			SysLib.rawread(blockId, pageTable[foundPage].dataBlock); 
-			pageTable[foundPage].blockFrameNumber 	= blockId;				
+			pageTable[foundPage].frame 	= blockId;				
 			pageTable[foundPage].refBit 			= true;	
 			System.arraycopy(pageTable[foundPage].dataBlock, 0, buffer, 0, blockSize);	
 			return true;
 		}
 		writeBack(nextVictim());	
 		SysLib.rawread(blockId, pageTable[newVictim].dataBlock);	     	
-		pageTable[newVictim].blockFrameNumber 	= blockId;				
-		pageTable[newVictim].refBit 			= true;
+		pageTable[newVictim].frame 	= blockId;				
+		pageTable[newVictim].refBit = true;
 		System.arraycopy(pageTable[newVictim].dataBlock, 0, buffer, 0, blockSize);
 		return true;
 	}
@@ -99,24 +99,24 @@ public class Cache {
 		foundPage = findBlock(blockId);					
 		if(foundPage != -1) { 	
 			System.arraycopy(buffer, 0, pageTable[foundPage].dataBlock, 0, blockSize);
-			pageTable[foundPage].blockFrameNumber 	= blockId;
-			pageTable[foundPage].dirtyBit 			= true;						
-			pageTable[foundPage].refBit 			= true;	
+			pageTable[foundPage].frame 		= blockId;
+			pageTable[foundPage].dirtyBit 	= true;						
+			pageTable[foundPage].refBit 	= true;	
 			return true; 
 		}	
 
 		foundPage = findFreePage();					
 		if(foundPage != -1) { 	
 			System.arraycopy(buffer, 0, pageTable[foundPage].dataBlock, 0, blockSize);
-			pageTable[foundPage].blockFrameNumber 	= blockId;	
-			pageTable[foundPage].dirtyBit 			= true;						
-			pageTable[foundPage].refBit 			= true;	
+			pageTable[foundPage].frame 		= blockId;	
+			pageTable[foundPage].dirtyBit 	= true;						
+			pageTable[foundPage].refBit 	= true;	
 			return true; 
 		}
 		writeBack(nextVictim());
-		pageTable[newVictim].blockFrameNumber 	= blockId;
-		pageTable[newVictim].dirtyBit 			= true;							
-		pageTable[newVictim].refBit 			= true;
+		pageTable[newVictim].frame 			= blockId;
+		pageTable[newVictim].dirtyBit 		= true;							
+		pageTable[newVictim].refBit 		= true;
 		System.arraycopy(buffer, 0, pageTable[newVictim].dataBlock, 0, blockSize);
 		return true;		
     }
@@ -131,8 +131,8 @@ public class Cache {
 	public synchronized void flush() {
 		for(int i = 0; i < pageTable.length; i++) { 	
 			writeBack(i);
-			pageTable[i].blockFrameNumber 	= -1;			
-			pageTable[i].refBit 			= false;		
+			pageTable[i].frame 	= -1;			
+			pageTable[i].refBit = false;		
 		}
 		SysLib.sync();
     }
--- ThreadOS/Test4.java
+++ ThreadOS/Test4.java
@@ -4,12 +4,12 @@ import java.util.Date;
 import java.util.Random;
 
 public class Test4 extends Thread {
-	private int cachedBlocks 		= 10;	
-	private int arrayTest 			= 200;	
-	private int diskBlockSize 		= 512;	
+	private int cachedBlocks 	= 10;	
+	private int arrayTest 		= 200;	
+	private int diskBlockSize 	= 512;	
 	private int testing;
-	private String status 			= "disabled";
-	private boolean cacheEnabled 	= false;	
+	private String status 		= "disabled";
+	private boolean enabled 	= false;	
 	private byte[] readBytes;	
 	private byte[] writeBytes;	
 	private Random random;
@@ -18,7 +18,7 @@ public class Test4 extends Thread {
     private long stopTime;                            
 
     private void getPerformance(String testName) {
-    	if (cacheEnabled == true) {
+    	if (enabled == true) {
 	      SysLib.cout("Test: " + testName + " | Cache: Enabled | " 	+ (stopTime - startTime) + "ms \n");
 	    } else {
 	      SysLib.cout("Test: " + testName + " | Cache: Disabled | " + (stopTime - startTime) + "ms \n");
@@ -31,10 +31,10 @@ public class Test4 extends Thread {
 		random 		= new Random();
 
 		if(args[0].equals("enabled")) {
-			cacheEnabled  	= true; 
+			enabled  	= true; 
 			status 			= "enabled";	
 		} else {
-			cacheEnabled  	= false;	 
+			enabled  	= false;	 
 			status 			= "disabled"; 
 		}
 
@@ -72,7 +72,7 @@ public class Test4 extends Thread {
 			}	      			  			  			  			  			
     	}
 		
-		if(cacheEnabled) {
+		if(enabled) {
 			SysLib.csync();		
 		} else {
 			SysLib.sync();
@@ -80,16 +80,16 @@ public class Test4 extends Thread {
 		SysLib.exit( );                               
     }
 
-	private void reader(int blockId, byte[] buffer) {
-		if (cacheEnabled) {
+	private void read(int blockId, byte[] buffer) {
+		if (enabled) {
 			SysLib.cread(blockId, buffer);
 		} else {
 			SysLib.rawread(blockId, buffer);
 		}
 	}
 	
-	private void writer(int blockId, byte[] buffer) {
-		if (cacheEnabled) {
+	private void write(int blockId, byte[] buffer) {
+		if (enabled) {
 			SysLib.cwrite(blockId, buffer);
 		} else {
 			SysLib.rawwrite(blockId, buffer);
@@ -105,11 +105,11 @@ public class Test4 extends Thread {
 		}
 
 		for (int i = 0; i < arrayTest; i++) {
-			writer(randomAccessArr[i], writeBytes); 
+			write(randomAccessArr[i], writeBytes); 
 		}
 
 		for(int i = 0; i < arrayTest; i++) {	
-			reader(randomAccessArr[i], readBytes);
+			read(randomAccessArr[i], readBytes);
 		}
 
 		if(!(Arrays.equals(writeBytes, readBytes))) {
@@ -119,17 +119,15 @@ public class Test4 extends Thread {
 	}
 
 	private void localizedAccess() {    
-		random.nextBytes(writeBytes); 
-        
         for(int i = 0; i < arrayTest; i++) {                
-          for(int j = 0; j < cachedBlocks; j++)	{                  
-			writer(j, writeBytes); 
+          	for(int j = 0; j < cachedBlocks; j++)	{                  
+				write(j, writeBytes); 
 			}
 		}	
 
         for(int i = 0; i < arrayTest; i++) {                
         	for(int j = 0; j < cachedBlocks; j++) {  
-				reader(j, readBytes);
+				read(j, readBytes);
 			}
         }
 	
@@ -139,8 +137,7 @@ public class Test4 extends Thread {
 		}
     }
 
-	private void mixedAccess()
-	{ 
+	private void mixedAccess() { 
 		random.nextBytes(writeBytes);
 		int[] mixedAccessArr = new int[arrayTest];            
         for(int i = 0; i < arrayTest; i++) {   
@@ -152,11 +149,11 @@ public class Test4 extends Thread {
         }
 		
         for(int i = 0; i < arrayTest; i++) {    
-             writer(mixedAccessArr[i], writeBytes);              
+             write(mixedAccessArr[i], writeBytes);              
         }		
         
         for(int i = 0; i < arrayTest; i++) {            
-            reader(mixedAccessArr[i], readBytes);                         
+            read(mixedAccessArr[i], readBytes);                         
         }		
 		
 		if(!(Arrays.equals(writeBytes, readBytes))) {
@@ -166,14 +163,12 @@ public class Test4 extends Thread {
     }
 
 	private void adversaryAccess() {	
-		random.nextBytes(writeBytes); 
-     		
         for (int i = cachedBlocks; i < diskBlockSize; i++) {                                 
-            writer(i, writeBytes);                  
+            write(i, writeBytes);                  
         }        
                  
         for (int i = cachedBlocks; i < diskBlockSize; i++) {              
-            reader(i, readBytes);                             
+            read(i, readBytes);                             
         }        		
 		
 		if(!(Arrays.equals(writeBytes, readBytes))) {
