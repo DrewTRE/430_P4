4299b2e6a1dc34af68496b634571b5f7d83d3e65
DrewTRE <43226138+DrewTRE@users.noreply.github.com>
2019-05-27 17:02:39 -0700

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- ThreadOS/Test4.java
+++ ThreadOS/Test4.java
@@ -22,10 +22,10 @@ public class Test4 extends Thread {
 	private Random random;
 	private long startTime;                           
     private long stopTime;  
-    private long startRead; 
-    private long stopRead; 
-    private long startWrite; 
-    private long stopWrite;                           
+    private float startRead; 
+    private float stopRead; 
+    private float startWrite; 
+    private float stopWrite;                           
 
     // Return Performance of each Test. 
     private void getPerformance(String testName) {
